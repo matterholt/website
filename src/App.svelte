9ab3555b65ffdf4aeff423f9128d1a08210abb78
MAtterholt <matterholt84@gmail.com>
2021-01-06 20:37:20 -0500

    more play

--- src/App.svelte
+++ src/App.svelte
@@ -2,22 +2,35 @@
     export let pageTitle;
 
     import DailyTimeLog from './components/DailyTimeLog.svelte'
+    let activeDays = ['Monday','Tuesday','Wednesday','Thursday','Friday']
 
     let timeLogs =[
-        {day:"Monday", punchIn:"08:00", punchOut:"16:30" },
-        {day:"Tuesday", punchIn:"08:00", punchOut:"16:30" },
-        {day:"Wednesday", punchIn:"08:00", punchOut:"16:30" },
-        {day:"Thursday", punchIn:"08:00", punchOut:"16:30" },
-        {day:"Friday", punchIn:"08:00", punchOut:"16:30" },
-]
+        {id:1, day:"Monday", punchIn:"08:00", punchOut:"16:30" },
+        {id:2,day:"Tuesday", punchIn:"08:00", punchOut:"16:30" },
+        {id:3,day:"Wednesday", punchIn:"08:00", punchOut:"16:30" },
+        {id:4,day:"Thursday", punchIn:"08:00", punchOut:"16:30" },
+        {id:5,day:"Friday", punchIn:"08:00", punchOut:"16:30" },
+        ]
+
+    let totalHours = 0
+
+        function calculateDailyHours (timeIn,timeOut){
+            
+
+        }
 
 </script>
 <style>
 
 </style>
 <h1>{pageTitle}</h1>
+
 <main>
-    <h2>Building web with svelte</h2>
+<div class="timeLogHead">
+    <h2>Total Hours: <span>{timeLogs}</span></h2>
+    
+
+</div>
 
 {#each timeLogs as timeLog}
  <DailyTimeLog timeLog={timeLog}/>
--- src/components/DailyTimeLog.svelte
+++ src/components/DailyTimeLog.svelte
@@ -1,5 +1,45 @@
 <script>
     export let timeLog;
+
+
+   $:todayHours = calculateDailyHours(timeLog.punchIn,timeLog.punchOut)
+
+
+
+
+    function timeConvertToMinutes(time) {
+    const [hr, min] = time.split(":");
+    const hrToMin = Number(hr) * 60;
+    const totalMin = hrToMin + Number(min);
+    return totalMin;
+    }
+
+    function calHourPass(inTime, outTime, lunchTime = 0.5) {
+    let totalTime = outTime - inTime;
+    let todayHour = totalTime / 60 - lunchTime;
+    return todayHour.toFixed(1);
+    }
+
+    function calculateDailyHours(dailyClockIn, dailyClockOut) {
+    const arriveTimeMins = timeConvertToMinutes(dailyClockIn);
+    const leaveTimeMins = timeConvertToMinutes(dailyClockOut);
+
+    return calHourPass(arriveTimeMins, leaveTimeMins);
+    }
+
+        function updateValue (event){
+        let currentElemId = event.target.id
+        let currentElemName = event.target.name
+
+        let currentElemValue = event.target.value
+
+        console.log(`update value on ${currentElemName} with value of ${currentElemValue}`)
+        timeLog = {...timeLog, [currentElemName]:currentElemValue}
+        console.log(timeLog)
+    }
+
+
+
 </script>
 <style>
 
@@ -10,9 +50,14 @@
 
 <div class="timeLog">
         <h3>{timeLog.day}</h3>
+
         <label for="clockIn">Clock In Time</label>
-       <input type="time" name="clockIn" id="clockIn" value={timeLog.punchIn} />
-        
+       <input type="time" name="clockIn" id={`clockIn_${timeLog.day}`} value={timeLog.punchIn}  on:change={updateValue}/>
+
+
        <label for="clockOut">Clock Out Time</label>
-       <input type="time" name="clockOut" id="clockOut" value={timeLog.punchOut} />
+       <input type="time" name="clockOut" id={`clockOut_${timeLog.day}`} value={timeLog.punchOut} on:change={updateValue}/>
+
+       <h3>Today's hours {todayHours}</h3>
+
     </div>
